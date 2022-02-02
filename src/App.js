d5a820eb6ea874284c7f863fcbea0e674408b5ed
Caio Santos <caio.santos@msn.com>
2022-02-02 18:35:40 +0900

    add components app.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,18 @@ import Map from './components/Map/Map';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
