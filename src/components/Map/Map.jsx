0ec4042a92c49d668d1ba9cfb6ca3578620c3752
Caio Santos <caio.santos@msn.com>
2022-02-03 10:07:46 +0900

    add import at map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles';
 
 const Map = () => {
   return (
