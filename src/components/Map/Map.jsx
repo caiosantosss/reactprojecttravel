b30bbcf7941edf260bdc2e6a97aa04a37393ffa3
Caio Santos <caio.santos@msn.com>
2022-02-04 19:20:15 +0900

    fix list cards

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -8,26 +8,26 @@ import useStyles from './styles';
 
 import mapStyles from './mapStyles';
 
-const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked, weatherData }) => {
+const Map = ({ setCoords, setBounds, coords, places, setChildClicked, weatherData }) => {
   const classes = useStyles();
-  const isDesktop = useMediaQuery('(max-width:600px)');
+  const isDesktop = useMediaQuery('(min-width:600px)');
 
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
-        defaultCenter={coordinates}
-        center={coordinates}
+        defaultCenter={coords}
+        center={coords}
         defaultZoom={14}
         marginBottom={[50, 50, 50, 50]}
         options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
         onChange={(e) => {
-          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setCoords({ lat: e.center.lat, lng: e.center.lng });
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
         onChildClick={(child) => { setChildClicked(child) }}
       >
-        {places?.map((place, i) => (
+        {places.length && places?.map((place, i) => (
           <div
             className={classes.markerContainer}
             lat={Number(place.latitude)}
@@ -35,21 +35,21 @@ const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked, 
             key={i}
           >
             {
-              !isDesktop ? (
+              !isDesktop ?
                 <LocationOnOutlinedIcon color="primary" fontSize="large" className={classes.marker} />
-              ) : (
-                <Paper elevation={3} className={classes.paper}>
-                  <Typography className={classes.typography} variant="subtitle2" gutterBottom>
-                    {place.name}
-                  </Typography>
-                  <img
-                    className={classes.pointer}
-                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
-                    alt={place.name}
-                  />
-                  <Rating size="small" value={Number(place.rating)} readOnly />
-                </Paper>
-              )
+                : (
+                  <Paper elevation={3} className={classes.paper}>
+                    <Typography className={classes.typography} variant="subtitle2" gutterBottom>
+                      {place.name}
+                    </Typography>
+                    <img
+                      className={classes.pointer}
+                      src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                      alt={place.name}
+                    />
+                    <Rating size="small" value={Number(place.rating)} readOnly />
+                  </Paper>
+                )
             }
           </div>
         ))}
