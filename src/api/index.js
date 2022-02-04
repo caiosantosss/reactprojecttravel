68bd6aeff721e59519e8e3a15587cc20bb472b44
Caio Santos <caio.santos@msn.com>
2022-02-04 20:46:03 +0900

    fix maps

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 import axios from 'axios';
 
 export const getPlacesData = async (type, sw, ne) => {
@@ -5,15 +6,16 @@ export const getPlacesData = async (type, sw, ne) => {
     const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
       params: {
         bl_latitude: sw.lat,
-        tr_latitude: ne.lat,
         bl_longitude: sw.lng,
         tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
       },
       headers: {
         'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
-        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com'
-      }
+        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+      },
     });
+
     return data;
   } catch (error) {
     console.log(error);
@@ -22,18 +24,18 @@ export const getPlacesData = async (type, sw, ne) => {
 
 export const getWeatherData = async (lat, lng) => {
   try {
-    const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/weather', {
-      params: {
-        lat: lat,
-        lon: lng,
-      },
-      headers: {
-        'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-        'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY
-      }
-    });
-    return data;
+    if (lat && lng) {
+      const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
+        params: { lat, lon: lng },
+        headers: {
+          'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+          'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+        },
+      });
+
+      return data;
+    }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,58 +4,52 @@ import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
 
-import useStyles from './styles';
-
 import mapStyles from './mapStyles';
+import useStyles from './styles.js';
 
-const Map = ({ setCoords, setBounds, coords, places, setChildClicked, weatherData }) => {
+const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherData }) => {
+  const matches = useMediaQuery('(min-width:600px)');
   const classes = useStyles();
-  const isDesktop = useMediaQuery('(min-width:600px)');
 
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
         defaultCenter={coords}
         center={coords}
         defaultZoom={14}
-        marginBottom={[50, 50, 50, 50]}
+        margin={[50, 50, 50, 50]}
         options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
         onChange={(e) => {
           setCoords({ lat: e.center.lat, lng: e.center.lng });
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
-        onChildClick={(child) => { setChildClicked(child) }}
+        onChildClick={(child) => setChildClicked(child)}
       >
-        {places.length && places?.map((place, i) => (
+        {places.length && places.map((place, i) => (
           <div
             className={classes.markerContainer}
             lat={Number(place.latitude)}
-            lng={Number(place.longitute)}
+            lng={Number(place.longitude)}
             key={i}
           >
-            {
-              !isDesktop ?
-                <LocationOnOutlinedIcon color="primary" fontSize="large" className={classes.marker} />
-                : (
-                  <Paper elevation={3} className={classes.paper}>
-                    <Typography className={classes.typography} variant="subtitle2" gutterBottom>
-                      {place.name}
-                    </Typography>
-                    <img
-                      className={classes.pointer}
-                      src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
-                      alt={place.name}
-                    />
-                    <Rating size="small" value={Number(place.rating)} readOnly />
-                  </Paper>
-                )
-            }
+            {!matches
+              ? <LocationOnOutlinedIcon color="primary" fontSize="large" />
+              : (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
+                  <img
+                    className={classes.pointer}
+                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                  />
+                  <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+                </Paper>
+              )}
           </div>
         ))}
-        {weatherData?.list?.map((data, i) => (
+        {weatherData?.list?.length && weatherData.list.map((data, i) => (
           <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
-            <img height={100} src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} />
+            <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="70px" />
           </div>
         ))}
       </GoogleMapReact>
